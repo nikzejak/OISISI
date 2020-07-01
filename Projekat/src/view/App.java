4019c95c1a27b44a2c4556a35e864cf2d8d8c874
nikzejak <nik.zejak@gmail.com>
2020-07-01 22:06:33 +0200

    fix_for_LookAndFeel

--- Projekat/src/view/App.java
+++ Projekat/src/view/App.java
@@ -7,7 +7,10 @@ public class App {
 
 	 public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException {
 		 
-		 //UIManager.setLookAndFeel("de.javasoft.plaf.synthetica.SyntheticaPlainLookAndFeel");
+		 String s = "de.javasoft.plaf.synthetica.SyntheticaPlainLookAndFeel";
+		 String s2 = "de.javasoft.plaf.synthetica.SyntheticaAluOxideLookAndFeel";
+		 
+		 UIManager.setLookAndFeel(s2);
 		 
 		 MainFrame.getInstance().setVisible(true);
 	 }
